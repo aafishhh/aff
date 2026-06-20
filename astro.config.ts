165eb2a3970d079c631c83ebdbb810383a95c084
aafishhh <161629206+aafishhh@users.noreply.github.com>
2026-06-20 20:35:41 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -25,6 +25,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  trailingSlash: 'always',
 
   integrations: [
     sitemap(),
